738b20d8fbe457f8eb2dc419ffbc9ddda25fe665
ivanjimenez <ivan.jimenezster@gmail.com>
2014-05-13 15:50:06 -0400

    lab2

---
+++ Lab2/base_thread.py
@@ -0,0 +1,13 @@
+import time
+import threading
+
+class CountdownThread(threading.Thread):
+    def __init__(self,count):
+        threading.Thrad.__init__(self)
+        self.count = count
+    def run(self):
+        while self.count > 0:
+            print "Counting down", self.count
+            self.count -= 1
+            time.sleep(5)
+        return
