67cbcf260b7f68631878452633371345ecfa93a9
ivan.jimenez <ivan.jimenezster@gmail.com>
2014-05-14 01:12:54 +0400

    second

--- sh/ej1.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Ejempo de script que recibe parámetros
-
-echo "El script $0"
-echo "Recibe los argumentos $1 $2 $3 $4"
-echo "Número de argumentos $#"
--- sh/ej2.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-RNM = `expr $RANDOM % 5`
-echo $RNM
