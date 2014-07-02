06a282c66bb6a0dfd40eda01aee5870bef8a96f5
ivan.jimenez <ivan.jimenezster@gmail.com>
2014-07-02 04:50:34 +0400

    first

---
+++ Lab4/script2.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import os
+# Comment
+''' hola
+
+qué 
+tal
+
+'''
+
+a = 2.0
+
+if a == 2.0:
+	print "Vale 2.0"
+elif a == 1.0:
+	print "Vale 1.0"
+else:
+	print "no vale nada"
+	
+if a == 2 and b == 1:
+		print "a es igual 2 y b es igual a 1"
+		
+x = 1
+while x < 4:
+		print "Vale 4"
+			
