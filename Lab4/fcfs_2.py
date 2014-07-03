721b588f334e3a3d7683c8c3c224f19efd0a62d2
ivan.jimenez <ivan.jimenezster@gmail.com>
2014-07-04 00:49:25 +0400

    Lab5

---
+++ Lab4/fcfs_2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+''' Implementación del algoritmo de Planificación de Disco FCFS'''
+
+def FCFS(peticiones, posicionInicial):
+
+
+#main
+if __name__ == '__main__':
+		peticiones = [2069, 1212, 2296, 2800, 544, 1618, 356, 1523, 4965, 3681]
+		posicionInicial = 1000
+		FCFS(requests, posicionInicial)
